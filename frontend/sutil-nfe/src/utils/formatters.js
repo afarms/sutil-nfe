63087dc0768bcf99b233caeabf4953fcba170aba
Rafael Moreira <rafaelsmoreiras@gmail.com>
2026-07-06 21:54:48 -0300

    feat: removed fronted

--- frontend/sutil-nfe/src/utils/formatters.js
+++
@@ -1,7 +0,0 @@
-export const formatCurrency = (value) => {
-  return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-};
-
-export const formatPercentage = (value) => {
-  return `${value.toFixed(0)}%`;
-};
